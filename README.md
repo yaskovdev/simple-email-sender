119b43eeab14e2f44e1e5ce6e93f9878a1947545
Sergey Yaskov <yaskovdev@gmail.com>
2024-12-10 20:14:39 +0200

    Add possibility to use HTML for emails.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. Follow [these steps](https://stackoverflow.com/a/32336).
 2. Copy `Config.Example.json` as `Config.json` (ignored in Git) and fill in the values.
+3. Check if you need the period at the end of the sentence in `EmailBody.html`! Depends on the name of the article.
--- SimpleEmailSender/SimpleEmailSender/Program.cs
+++ SimpleEmailSender/SimpleEmailSender/Program.cs
@@ -12,9 +12,9 @@ internal static class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
         var jsonFilePath = GetResourcePath("Resources/Config.json");
-        var body = File.ReadAllText(GetResourcePath("Resources/EmailBody.txt"));
+        var body = File.ReadAllText(GetResourcePath("Resources/EmailBody.html"));
+        var subject = "New Article: Intro to Genetic Programming: Can Evolution Write Computer Programs?";
         var jsonString = File.ReadAllText(jsonFilePath);
 
         var config = JsonSerializer.Deserialize<Config>(jsonString, new JsonSerializerOptions
@@ -27,35 +27,39 @@ internal static class Program
         Console.WriteLine($"Test recipients: {string.Join(", ", config.TestRecipients)}");
         Console.WriteLine($"Recipients: {string.Join(", ", config.Recipients)}");
 
-        using var smtpClient = new SmtpClient
-        {
-            Host = "smtp.gmail.com",
-            Port = 587,
-            EnableSsl = true,
-            DeliveryMethod = SmtpDeliveryMethod.Network,
-            UseDefaultCredentials = false,
-            Credentials = new NetworkCredential(config.Sender, config.SenderPassword)
-        };
+        using var smtp = new SmtpClient();
+        smtp.Host = "smtp.gmail.com";
+        smtp.Port = 587;
+        smtp.EnableSsl = true;
+        smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+        smtp.UseDefaultCredentials = false;
+        smtp.Credentials = new NetworkCredential(config.Sender, config.SenderPassword);
+
         Console.WriteLine($"Sending to {config.TestRecipients.Count} test recipients...");
         foreach (var recipient in config.TestRecipients)
         {
-            var message = new MailMessage(config.Sender, recipient)
-            {
-                Subject = "New Article: Intro to Genetic Programming: Can Evolution Write Computer Programs?",
-                Body = body
-            };
             Console.WriteLine($"Sending email to test recipient {recipient}");
-            smtpClient.Send(message);
+            using var message = CreatEmailMessage(config.Sender, recipient, subject, body);
+            smtp.Send(message);
         }
 
         Console.WriteLine($"Sending to {config.Recipients.Count} real recipients...");
-        foreach (var recipient in config.TestRecipients)
+        foreach (var recipient in config.Recipients)
         {
             Console.WriteLine($"Sending email to real recipient {recipient}, confirm by pressing any key");
             Console.ReadKey();
-            smtpClient.Send(config.Sender, recipient, "subject", "body");
+            using var message = CreatEmailMessage(config.Sender, recipient, subject, body);
+            smtp.Send(message);
         }
     }
+
+    private static MailMessage CreatEmailMessage(string sender, string recipient, string subject, string body) =>
+        new(sender, recipient)
+        {
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true
+        };
 
     private static string GetResourcePath(string name)
     {
